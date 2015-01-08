39a8bbf1417b085b2738f2839d9373d9df2fe715
SamLaio <f85122@gmail.com>
2015-01-08 16:51:14 +0800

    v 1.5 captcha update:post msg with jquery and rewrite for jquery plugn Signed-off-by: SamLaio <f85122@gmail.com>

--- control/captcha.php
+++ control/captcha.php
@@ -10,26 +10,29 @@ class captcha {
 		$_SESSION['CaptchaPw'] = rand(50,500000);
 		$this->LibCaptcha->CreateImg($this->LibCaptcha->num2adb($_SESSION['CaptchaPw']));
 	}
-	public function ImgCheck($key){
-		if(isset($_SESSION['CatptchaError'])){
-			if($_SESSION['CatptchaError'] >= 3){
-				echo 0;
-				exit;
-			}
-		}
-		$key = $key['post']['captcha'];
-		if(isset($_SESSION['CaptchaPw'])){
-			// echo $_SESSION['CaptchaPw'].'---'.strtoupper($key);
-			if($this->LibCaptcha->CheckImg($_SESSION['CaptchaPw'],strtoupper($key))){
-				echo 1;
-			}else{
-				echo 0;
-				if(isset($_SESSION['CatptchaError'])){
-					$_SESSION['CatptchaError']++;
-				}else{
-					$_SESSION['CatptchaError'] = 1;
+	public function ImgCheck($key=false){
+		if($key){
+			if(isset($_SESSION['CatptchaError'])){
+				if($_SESSION['CatptchaError'] >= 3){
+					echo 0;
+					exit;
 				}
 			}
+			$key = $key['post']['captcha'];
+			if(isset($_SESSION['CaptchaPw'])){
+				// echo $_SESSION['CaptchaPw'].'---'.strtoupper($key);
+				if($this->LibCaptcha->CheckImg($_SESSION['CaptchaPw'],strtoupper($key))){
+					echo 1;
+				}else{
+					echo 0;
+					if(isset($_SESSION['CatptchaError'])){
+						$_SESSION['CatptchaError']++;
+					}else{
+						$_SESSION['CatptchaError'] = 1;
+					}
+				}
+			}else
+				echo 0;
 		}else
 			echo 0;
 	}
